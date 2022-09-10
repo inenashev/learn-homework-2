623941faa518e49439190bfe274d2945a6149d30
Ivan Nenashev <inenashev@icloud.com>
2022-09-10 13:47:39 +0300

    csv

--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -10,12 +10,25 @@
 
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    data = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+        {'name': 'Петр', 'age': 38, 'job': 'Small boss'}
+    ]
+    import csv
+
+    with open('data.csv','w',encoding='utf-8',newline='') as csv_file:
+        fields = data[0].keys()
+        writer = csv.DictWriter(csv_file, fields, delimiter=',')
+        for element in data:
+            writer.writerow(element)
 
 if __name__ == "__main__":
     main()
